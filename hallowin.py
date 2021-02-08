5e0c16c54209189bd8ac2812ff5492da8b8f342f
Surya Teja <msushanth55@gmail.com>
2021-02-08 10:46:54 +0530

    initial commit

--- hallowin.py
+++ hallowin.py
@@ -7,7 +7,7 @@ image = pygame.image.load('hallowin.jpg')
 window.blit(image, (0,0))
 pygame.display.update()
 sleep(4)
-#then second music will paly after 3sec
+#then second music will paly after 3seccods
 pygame.mixer.init()
 pygame.mixer.music.load('horror-nightmare.mp3')
 pygame.mixer.music.play()
