d52aba93dde403d51f7720ba21af7e602c5bad5f
Surya Teja <msushanth55@gmail.com>
2021-02-08 10:27:07 +0530

    initial commit

--- hallowin.py
+++ hallowin.py
@@ -2,7 +2,17 @@ import pygame
 from time import sleep
 pygame.init()
 window = pygame.display.set_mode((0,0), pygame.FULLSCREEN)
+#first image will display
 image = pygame.image.load('hallowin.jpg')
 window.blit(image, (0,0))
 pygame.display.update()
-sleep(10)
+sleep(4)
+#then second music will paly after 3sec
+pygame.mixer.init()
+pygame.mixer.music.load('horror-nightmare.mp3')
+pygame.mixer.music.play()
+sleep(6)
+#image = pygame.image.load('hallowin.jpg')
+#window.blit(image, (0,0))
+#pygame.display.update()
+#sleep(4)
